445c0ae5ca796ee0eb993dce038932adc4750827
Chris Wu <christian.wucy@gmail.com>
2019-07-26 22:36:42 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,17 @@
+products = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	# p = []
+	# p.append(name)
+	# p.append(price)
+	# 簡易法
+	products.append([name, price])
+print(products)
+
+# 第一個[0]代表大清單的第1格，
+# 第2個[0]代表大清單的第1格裡面的小清單的第1格
+products[0][0]
+
