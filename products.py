916bb46985eb118eb369f0d392c05bbb7641667e
Chris Wu <christian.wucy@gmail.com>
2019-07-27 17:06:38 +0800

    add header

--- products.py
+++ products.py
@@ -18,3 +18,10 @@ print(products)
 for p in products:
 	print(p[0], '的價格是', p[1])
 
+# 字串合併用加法 'abc' + '123' = 'abc123'
+# 字串也可以乘法 'abc' * 3 = 'abcabcabc'
+
+with open('products.csv', 'w', encoding = 'utf-8') as f: # 打開檔案, f.write寫入csv檔
+	f.write('商品,價格\n') # 先寫入欄位，並於上方加入encoding = 'utf-8' 解決中文亂碼問題
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n') # \n :換行
